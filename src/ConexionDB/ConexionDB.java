043c95ae6f41a1f2d478da036cc5f35e36fbf8a4
Jhodavid <65832961+Jhodavid@users.noreply.github.com>
2020-05-24 16:55:18 -0500

    Agregé Paquete y clase ConexionDB, agregé las librerias para la conexion con la DB

---
+++ src/ConexionDB/ConexionDB.java
@@ -0,0 +1,48 @@
+
+package ConexionDB;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jhoda
+ */
+
+public class ConexionDB {
+
+Connection ccn = null;
+Statement st = null;
+
+    public ConexionDB(){
+        try 
+            {
+                String rutafile = "C:\\Programar\\Usuarios\\jhoda\\Documentos\\NetBeansProjects\\Proyecto-WFH\\DateBaseWFH.accdb";
+                //Tienen que cambiar la ruta por la de ustedes.
+                String Url = "jdbc:ucanaccess://" + rutafile;
+                ccn = DriverManager.getConnection(Url);
+                st = ccn.createStatement();
+            } catch (SQLException e) 
+                {
+                    JOptionPane.showMessageDialog(null, "CONEXION ERRONEA " + e);  
+                }
+    }
+    
+    public Connection getConnection(){
+        return ccn;
+    }
+    
+    public void Desconexion(){
+        try 
+            {
+                ccn.close();            
+                System.exit(0);
+            } catch (SQLException ex) 
+                {
+                    Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+                }
+    }
+    
+}
