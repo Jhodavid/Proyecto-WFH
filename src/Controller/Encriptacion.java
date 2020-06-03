9554e4b8217b3839e8c4933c8fe994d87fa3567e
USUARIO <USUARIO@DESKTOP-SBO0DI8>
2020-06-03 16:27:26 -0500

    Cifrado de Datos ahora si

--- src/Controller/Encriptacion.java
+++ src/Controller/Encriptacion.java
@@ -1,4 +1,3 @@
-
 package Controller;
 
 /**
@@ -6,46 +5,99 @@ package Controller;
  * @author jhoda
  */
 public class Encriptacion {
-    public static void main(String[] args) {
-        
-    
 
-  String  pass="equisde";
-    char array[] = pass.toCharArray();
-    char arrayC[] = pass.toCharArray();
-    int V[]= new int [30];
-    for (int i=0;i<array.length;i++){
-         V[i]=(int) (Math.random()*12);
+    public String Cifrado(String pass) {
         
-        if((char)array[i]%2==0){
-        
-        arrayC[i]=(char)(array[i]+V[i]);
-        while(arrayC[i]>126){
-            arrayC[i]=(char)(array[i]-1);
+        char Array[] = pass.toCharArray();
+        int N = Array.length;
+        int C = N;
+        for (int i = 0; i < N; i++) {
+            Array[i] = (char) (Array[i] + (char) C);
+            C--;
+
         }
+        for (int i = 0; i < N - 1; i++) {
+            
+                char aux = Array[i];
+                Array[i] = Array[i + 1];
+                Array[i + 1] = aux;
+           
         }
-        if((char)array[i]%2!=0){
-        arrayC[i]=(char)(array[i]-V[i]-6);
-        while(arrayC[i]<32){
-        arrayC[i]=(char)(array[i]+1);}
-        
-        
+        for (int i = 0; i < N; i++) {
+
+            Array[i] = (char) (Array[i] + (char) i);
+
         }
-        System.out.println(V[i]);
+        for (int i = 0; i < N - 2; i++) {
+
+            char aux = Array[i];
+            Array[i] = Array[i + 2];
+            Array[i + 2] = aux;
+
+        }
+        for (int i = 0; i < N; i++) {
+            int k = 1 - i + 4;
+            Array[i] = (char) (Array[i] + (char) i + k);
+
+        }
+        for (int i = 0; i < N - 1; i++) {
+
+            char aux = Array[i];
+            Array[i] = Array[N - i - 1];
+            Array[N - i - 1] = aux;
+
+        }
+        String Encriptado = String.valueOf(Array);
+        return Encriptado;
     }
-    String cifrado= String.valueOf(arrayC);
-    char arrayD[]=cifrado.toCharArray();
-    for(int i=0;i<array.length;i++){
-        if((char)array[i]%2==0){
+
+    public String Desencriptado(String Encriptado) {
+        char array[] = Encriptado.toCharArray();
+        int N = array.length;
+        int C = N;
+        for (int i = 0; i < N; i++) {
+            char aux = array[i];
+            array[i] = array[N - i - 1];
+            array[N - i - 1] = aux;
+        }
+        for (int i = 0; i < N; i++) {
+            int k = 1 - i + 4;
+            array[i] = (char) (array[i] - ((char) (i + k)));
+
+        }
+        for (int i = 0; i < N - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                char aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+            }
+        }
+        for (int i = N - 1; i > 1; i--) {
+
+            char aux = array[i];
+            array[i] = array[i - 2];
+            array[i - 2] = aux;
+
+        }
+        for (int i = 0; i < N; i++) {
+
+            array[i] = (char) (array[i] - (char) i);
+
+        }
+        for (int i = N-1; i > 0; i--) {
+            
+                char aux = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = aux;
+           
+        }
         
-        arrayD[i]=(char)(arrayC[i]-V[i]);
-        
-        } 
-        if((char)array[i]%2!=0){
-        arrayD[i]=(char)(arrayC[i]+V[i]+6);
-        
+        for (int i = 0; i < N; i++) {
+            array[i] = (char) (array[i] - (char) C);
+            C--;
+
+        }
+String Dcifrado = String.valueOf(array);
+return Dcifrado;
     }
-        System.out.println(V[i]);
- }   String Dcifrado= String.valueOf(arrayD);
-     System.out.println(cifrado+"\n"+Dcifrado);
-}}
+}
