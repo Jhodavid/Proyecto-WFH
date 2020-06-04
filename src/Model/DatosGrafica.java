4277b3d397ff4ce3afcb7af350c13cebc4e05d3c
Jhodavid <65832961+Jhodavid@users.noreply.github.com>
2020-06-03 22:15:48 -0500

    Camilo es un mk hp

--- src/Model/DatosGrafica.java
+++ src/Model/DatosGrafica.java
@@ -12,23 +12,57 @@ package Model;
 public class DatosGrafica {
     
     public Enfermedad LI;
-    public DatosPesoOptimo PesoOptimo;
-    public DatosSobrePeso SobrePeso;
-    public DatosPorDebajo PorDebajo;
+    public int PesoOptimo1;
+    public int PesoOptimo2;
+    public int PesoOptimo3;
+    public int PesoOptimo4;
+    public int PesoOptimo5;
+    public int PesoOptimo6;
+    public int PesoOptimo7;
+    public int PesoOptimo8;
+    public int PesoOptimo9;
+    
+    public int SobrePeso1;
+    public int SobrePeso2;
+    public int SobrePeso3;
+    public int SobrePeso4;
+    public int SobrePeso5;
+    public int SobrePeso6;
+    public int SobrePeso7;
+    public int SobrePeso8;
+    public int SobrePeso9;
+    
+    public int PorDebajo1;
+    public int PorDebajo2;
+    public int PorDebajo3;
+    public int PorDebajo4;
+    public int PorDebajo5;
+    public int PorDebajo6;
+    public int PorDebajo7;
+    public int PorDebajo8;
+    public int PorDebajo9;
+    
     public Enfermedad LD;
 
     public DatosGrafica() {
-        PesoOptimo = null;
-        SobrePeso = null;
-        PorDebajo = null;
         LI = null;
         LD = null;
     }
 
     public DatosGrafica(DatosGrafica datos) {
-        PesoOptimo = null;
-        SobrePeso = null;
-        PorDebajo = null;
+        PesoOptimo1 = datos.PesoOptimo1;
+        PesoOptimo2 = datos.PesoOptimo2;
+        PesoOptimo3 = datos.PesoOptimo3;
+        PesoOptimo4 = datos.PesoOptimo4;
+        PesoOptimo5 = datos.PesoOptimo5;
+        PesoOptimo6 = datos.PesoOptimo6;
+        PesoOptimo7 = datos.PesoOptimo7;
+        PesoOptimo8 = datos.PesoOptimo8;
+        PesoOptimo9 = datos.PesoOptimo9;
+        
+        SobrePeso1 = datos.SobrePeso1;
+        
+        
         LI = null;
         LD = null;
     }
--- src/View/IFrmEstadisticasSaludFisica.java
+++ src/View/IFrmEstadisticasSaludFisica.java
@@ -28,9 +28,9 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
 
         initComponents();
     }
-
+/*
     public DefaultCategoryDataset GraficaSaludFisica() {
-
+        /*
         while (op.R != null) {
 
             Dato.addValue(R.PesoOptimo1, "Peso Optimo", "1-10 año(s)");
@@ -67,7 +67,7 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
         }
         return Dato;
     }
-
+        */
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -104,7 +104,6 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
         jPopupMenu5 = new javax.swing.JPopupMenu();
         jPopupMenu6 = new javax.swing.JPopupMenu();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
@@ -157,13 +156,6 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Estadistica de Salud Fisica");
 
-        jButton2.setText("GRAFICA DE ENFERMEDADES");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         jButton3.setText("GRAFICA PESOS");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -176,15 +168,13 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(115, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(112, 112, 112))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-                .addComponent(jButton2)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -192,17 +182,15 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(40, 40, 40))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Grafica = ChartFactory.createBarChart3D("GRAFICA DE PESO ", " RANGO DE EDADES", "CANTIDAD DE PERSONAS",
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        /*Grafica = ChartFactory.createBarChart3D("GRAFICA DE PESO ", " RANGO DE EDADES", "CANTIDAD DE PERSONAS",
                  GraficaSaludFisica(), PlotOrientation.VERTICAL, true, true, false);
         ChartPanel Panel = new ChartPanel(Grafica);
         JFrame Ventana;
@@ -210,11 +198,7 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
         Ventana.getContentPane().add(Panel);
         Ventana.pack();
         Ventana.setVisible(true);
-        Ventana.setLocationRelativeTo(null);
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        Ventana.setLocationRelativeTo(null);*/
     }//GEN-LAST:event_jButton3ActionPerformed
 
 
@@ -223,7 +207,6 @@ public class IFrmEstadisticasSaludFisica extends javax.swing.JInternalFrame {
     private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.ButtonGroup buttonGroup3;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JCheckBox jCheckBox2;
